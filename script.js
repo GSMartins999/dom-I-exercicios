06be6b4c2356c1c8b087f31897799f8bca1a279e
Giovanni Souza <GSMartins999>
2023-08-06 11:21:26 -0300

    domexercicios finalizado

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const frutas = ["laranja", "limão", "uva"]
 
-for(let i = 0; i < frutas.length; i++){
-    const fruta = frutas[i]
-    const adicionando = document.getElementById(`fruta-${i + 1}`)
-    adicionando.innerHTML = fruta;
-}
+//questão 1
+
+const adicionarFrutas1 = document.getElementById("fruta-1")
+adicionarFrutas1.innerHTML = frutas[0]
 
 
-// questao 2
+const adicionarFrutas2 = document.getElementById("fruta-2")
+adicionarFrutas2.innerHTML = frutas[1]
+
+
+const adicionarFrutas3 = document.getElementById("fruta-3")
+adicionarFrutas3.innerHTML = frutas[2]
+
+
+const adicionarFrutas4 = document.getElementById("fruta-4")
+adicionarFrutas4.innerHTML = frutas[4]
+
+
+//questão 2 - h0tão
 
 const exemplo = document.getElementById("nomeFruta")
 
@@ -17,7 +28,7 @@ const adicionandoFrutas =()=>{
 
 
 
-//questao 3
+//questao 3 - adicionando a fruta 4
 
 const adicionando = document.getElementById("fruta-4");
 
